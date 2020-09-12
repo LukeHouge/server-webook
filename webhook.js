aa17813bfaadf68d8d86a47e01c9bffdf98d513a
Luke Houge <lukehouge@gmail.com>
2020-09-12 18:15:43 -0500

    adding webhook.js

---
+++ webhook.js
@@ -0,0 +1,71 @@
+/*
+Created from this guide https://www.digitalocean.com/community/tutorials/how-to-use-node-js-and-github-webhooks-to-keep-remote-projects-in-sync 
+*/
+let prodDirectory = "/var/www/badgerloop.com";
+let devDirectory = "/var/www/dev.badgerloop.com";
+let lukeDirectory = "/var/www/lukehouge.com";
+let aiDirectory = "var/www/ai/build";
+let creds = require("./creds.json");
+let port = 3001;
+
+let http = require('http');
+let crypto = require('crypto');
+let fs = require('fs');
+
+const exec = require('child_process').exec;
+
+http.createServer((req, res) => {
+        console.log("req received");
+        let body = "";  
+
+        req.on('data', (chunk) => {
+                body += chunk;
+        });
+
+        req.on('end', () => {
+                console.log(body);
+                let sig = "sha1=" + crypto.createHmac('sha1', creds.secret).update(body).digest('hex');
+
+                let jsonData = JSON.parse(body);
+                let ref = jsonData.ref.split("/");
+                let repo = jsonData.repository.name;
+                if (repo === "website-condor") {
+                    if (ref[ref.length - 1] === "dev") {
+                        console.log("Badgerloop.com dev");
+                        if (req.headers['x-hub-signature'] === sig) {
+                                exec(`cd ${devDirectory} && git pull`, (err, stdout, stderr) => {
+                                //      if (err) // TODO: report error.
+                                });
+                        }
+                    } else if (ref[ref.length - 1] === "master") {
+                            console.log("Badgerloop.com prod")
+                            if (req.headers['x-hub-signature'] === sig) {
+                                    console.log("executing master pull");
+                                    exec(`cd ${prodDirectory} && git pull`, (err, stdout, stderr) => {
+                                            if (err) console.log(err);
+                                    });
+                            }
+                    }
+                }
+                else if (repo === "Personal-Site") {
+                        console.log("lukehouge.com");
+                        if (req.headers['x-hub-signature'] === sig) {
+                                exec(`cd ${lukeDirectory} && git pull`, (err, stdout, stderr) => {
+                                //      if (err) // TODO: report error.
+                                });
+                        }
+                }
+                else if (repo === "AI-UW-Website") {
+                        console.log("ai.cs.wisc.edu");
+                        if (req.headers['x-hub-signature'] === sig) {
+                                exec(`cd ${aiDirectory} && git pull && npm run build`, (err, stdout, stderr) => {
+                                //      if (err) // TODO: report error.
+                                });
+                        }
+                }
+                
+        });
+
+        res.end();
+
+}).listen(port);
